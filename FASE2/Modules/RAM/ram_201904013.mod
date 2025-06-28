a8130ca95145a56c8e442d107c216f8ed9cf8b45
ifigueroa065 <3059977500302@ingenieria.usac.edu.gt>
2025-06-27 22:55:23 -0600

    [UPDATE] - Se actualiza el recolector de métricas

---
+++ FASE2/Modules/RAM/ram_201904013.mod
@@ -0,0 +1,1 @@
+/home/figueroa/Documentos/SO1/FASE1/Proyecto1_Fase1_SO1_201904013/FASE2/Modules/RAM/ram_201904013.o
--- FASE2/Recolector/main.go
+++ FASE2/Recolector/main.go
@@ -1,19 +1,16 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
 	"log"
-	"net/http"
-	"os"
+	"fmt"
 	"time"
-
+	"sync"
+	"io/ioutil"
+	"encoding/json"
 	"github.com/gofiber/fiber/v2"
 )
 
-// Definir estructuras para los datos de los módulos
+// Estructuras para las métricas
 type RamInfo struct {
 	Total      uint64 `json:"total"`
 	Libre      uint64 `json:"libre"`
@@ -29,11 +26,11 @@ type CpuInfo struct {
 }
 
 type ProcesosInfo struct {
-	ProcesosCorriendo  uint64 `json:"procesos_corriendo"`
-	TotalProcesos      uint64 `json:"total_procesos"`
-	ProcesosDurmiendo  uint64 `json:"procesos_durmiendo"`
-	ProcesosZombie     uint64 `json:"procesos_zombie"`
-	ProcesosParados    uint64 `json:"procesos_parados"`
+	ProcesosCorriendo uint64 `json:"procesos_corriendo"`
+	TotalProcesos     uint64 `json:"total_procesos"`
+	ProcesosDurmiendo uint64 `json:"procesos_durmiendo"`
+	ProcesosZombie    uint64 `json:"procesos_zombie"`
+	ProcesosParados   uint64 `json:"procesos_parados"`
 }
 
 type MetricsPayload struct {
@@ -43,52 +40,60 @@ type MetricsPayload struct {
 }
 
 // Canal de métricas
-var metricsChan = make(chan MetricsPayload, 1)
+var metricsChan = make(chan MetricsPayload, 10)
 
 // Función para leer los archivos JSON generados por los módulos
 func leerArchivoJSON(path string, target interface{}) error {
 	data, err := ioutil.ReadFile(path)
 	if err != nil {
+		log.Printf("Error leyendo archivo %s: %v\n", path, err)
 		return err
 	}
-	return json.Unmarshal(data, target)
+	if err := json.Unmarshal(data, target); err != nil {
+		log.Printf("Error deserializando archivo %s: %v\n", path, err)
+		return err
+	}
+	return nil
 }
 
 // Función recolectora de RAM
-func recolectorRam(ch chan<- RamInfo) {
+func recolectorRam(ch chan<- RamInfo, wg *sync.WaitGroup) {
+	defer wg.Done()
 	for {
 		var ram RamInfo
-		err := leerArchivoJSON("/proc/ram_201904013", &ram)
-		if err != nil {
+		if err := leerArchivoJSON("/proc/ram_201904013", &ram); err != nil {
 			log.Println("Error leyendo módulo RAM:", err)
+		} else {
+			ch <- ram
 		}
-		ch <- ram
 		time.Sleep(5 * time.Second)
 	}
 }
 
 // Función recolectora de CPU
-func recolectorCpu(ch chan<- CpuInfo) {
+func recolectorCpu(ch chan<- CpuInfo, wg *sync.WaitGroup) {
+	defer wg.Done()
 	for {
 		var cpu CpuInfo
-		err := leerArchivoJSON("/proc/cpu_201904013", &cpu)
-		if err != nil {
+		if err := leerArchivoJSON("/proc/cpu_201904013", &cpu); err != nil {
 			log.Println("Error leyendo módulo CPU:", err)
+		} else {
+			ch <- cpu
 		}
-		ch <- cpu
 		time.Sleep(5 * time.Second)
 	}
 }
 
 // Función recolectora de Procesos
-func recolectorProcesos(ch chan<- ProcesosInfo) {
+func recolectorProcesos(ch chan<- ProcesosInfo, wg *sync.WaitGroup) {
+	defer wg.Done()
 	for {
 		var procesos ProcesosInfo
-		err := leerArchivoJSON("/proc/procesos_201904013", &procesos)
-		if err != nil {
+		if err := leerArchivoJSON("/proc/procesos_201904013", &procesos); err != nil {
 			log.Println("Error leyendo módulo Procesos:", err)
+		} else {
+			ch <- procesos
 		}
-		ch <- procesos
 		time.Sleep(5 * time.Second)
 	}
 }
@@ -108,63 +113,38 @@ func unificarMetricas(ramCh <-chan RamInfo, cpuCh <-chan CpuInfo, procesosCh <-c
 	}
 }
 
-// Función para enviar las métricas a la API
-func enviarAPI() {
-	apiHost := os.Getenv("API_HOST")
-	apiPort := os.Getenv("API_PORT")
-	apiURL := fmt.Sprintf("http://%s:%s/metrics", apiHost, apiPort)
-
-	for {
-		time.Sleep(5 * time.Second)
-		metrics := <-metricsChan
-
-		jsonData, err := json.Marshal(metrics)
-		if err != nil {
-			log.Println("Error serializando JSON:", err)
-			continue
-		}
-
-		resp, err := http.Post(apiURL, "application/json", bytes.NewBuffer(jsonData))
-		if err != nil {
-			log.Println("Error enviando a la API:", err)
-			continue
-		}
-
-		defer resp.Body.Close()
-		log.Println("Métricas enviadas correctamente ✅")
-	}
-}
-
 func main() {
-	ramCh := make(chan RamInfo)
-	cpuCh := make(chan CpuInfo)
-	procesosCh := make(chan ProcesosInfo)
+	var wg sync.WaitGroup
+	ramCh := make(chan RamInfo, 10)
+	cpuCh := make(chan CpuInfo, 10)
+	procesosCh := make(chan ProcesosInfo, 10)
 
-	go recolectorRam(ramCh)
-	go recolectorCpu(cpuCh)
-	go recolectorProcesos(procesosCh)
+	// Iniciar los recolectores en goroutines
+	wg.Add(3)
+	go recolectorRam(ramCh, &wg)
+	go recolectorCpu(cpuCh, &wg)
+	go recolectorProcesos(procesosCh, &wg)
 	go unificarMetricas(ramCh, cpuCh, procesosCh)
-	go enviarAPI()
 
+	// Iniciar servidor Fiber
 	app := fiber.New()
 
 	// Endpoint para mostrar métricas
 	app.Get("/metrics", func(c *fiber.Ctx) error {
 		metrics := <-metricsChan
-		return c.JSON(metrics)
+		return c.JSON(metrics)  // Aquí se expone el JSON de métricas a quien haga la petición GET
 	})
 
-	// Endpoint raíz para verificar que el servicio está en ejecución
+	// Endpoint raíz
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Recolector en ejecución. Endpoint disponible en /metrics ✅ ")
 	})
 
-	// Iniciar servidor
-	port := os.Getenv("RECOLECTOR_PORT")
-	if port == "" {
-		port = "8080"
-	}
-
+	// Iniciar el servidor
+	port := "8080" // Puerto fijo para pruebas
 	fmt.Printf("Servidor Fiber escuchando en http://localhost:%s\n", port)
 	log.Fatal(app.Listen(":" + port))
+
+	// Esperar a que los recolectores terminen (aunque en este caso, el programa sigue corriendo)
+	wg.Wait()
 }
